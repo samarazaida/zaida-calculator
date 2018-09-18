cf7f78f5a5562e86c7631f6cb193be7ed5d52e7e
samarazaida <samarazaida@gmail.com>
2018-09-17 21:06:34 -0300

    Add mult method

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ const soma = (a, b) => a + b;
 
 const subtracao = (a, b) => a - b;
 
-// const multiplicacao = (a, b) => a * b;
+const multiplicacao = (a, b) => a * b;
 
 // const divisao = (a, b) => a / b;
 
 export default {
   soma,
   subtracao,
-  // multiplicacao,
+  multiplicacao,
   // divisao,
 };
