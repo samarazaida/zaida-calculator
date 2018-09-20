ccd867b1d4cd14ead6b529da30e418bca5c48aec
samarazaida <39313243+samarazaida@users.noreply.github.com>
2018-09-19 23:27:14 -0300

    Add div mult methods (#10)
    feat: add mult method
    feat: add div method

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ const soma = (a, b) => a + b;
 
 const subtracao = (a, b) => a - b;
 
-// const multiplicacao = (a, b) => a * b;
+const multiplicacao = (a, b) => a * b;
 
-// const divisao = (a, b) => a / b;
+const divisao = (a, b) => a / b;
 
 // const pot = (a, b) => a ** b;
 
 export {
   soma,
   subtracao,
-//   multiplicacao,
-//   divisao,
+  multiplicacao,
+  divisao,
 //   pot,
 };
