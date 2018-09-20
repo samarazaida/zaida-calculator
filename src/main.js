3af38a142adaf29d9d7f77153a2510c4805e39e2
samarazaida <samarazaida@gmail.com>
2018-09-19 23:25:29 -0300

    feat: add mult method

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const soma = (a, b) => a + b;
 
 const subtracao = (a, b) => a - b;
 
-// const multiplicacao = (a, b) => a * b;
+const multiplicacao = (a, b) => a * b;
 
 // const divisao = (a, b) => a / b;
 
@@ -11,7 +11,7 @@ const subtracao = (a, b) => a - b;
 export {
   soma,
   subtracao,
-//   multiplicacao,
+  multiplicacao,
 //   divisao,
 //   pot,
 };
